71a52b312beb9ba86111571edb465d624c0dedb1
Raymond Zou <raymond.1zou@gmail.com>
2022-08-16 18:26:12 -0700

    add game state logic, add reset button

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
 const gameBoard = (() => {
-    let _markers = ['', '', '',
-                    '', '', '',
-                    '', '', ''];
-    let htmlBoxes = document.querySelectorAll('.box');
-    let htmlBoxesArr = Array.prototype.slice.call(htmlBoxes);
-    htmlBoxes.forEach(box => {
+    let _board = [null, null, null,
+                    null, null, null,
+                    null, null, null];
+
+    let _htmlBoxes = document.querySelectorAll('.box');
+    _htmlBoxes.forEach(box => {
         box.addEventListener('click', () => {
-            const index = htmlBoxesArr.indexOf(box);
-            if (_markers[index] == '') {
-                _markers[index] = gameManager.player.getMarker();
-                gameManager.player.flipMarker();
+            const index = Array.from(_htmlBoxes).indexOf(box);
+            if (!_board[index]) {
+                _board[index] = gameManager.player.getMarker();
                 renderBoard();
+                checkGameState();
+                gameManager.player.flipMarker();
             }
         })
     })
+
+    const checkGameState = () => {
+        // check rows
+        if (_board[0]) checkTriplet(0, 1, 2);
+        if (_board[3]) checkTriplet(3, 4, 5);
+        if (_board[6]) checkTriplet(6, 7, 8);
+        // check columns
+        if (_board[0]) checkTriplet(0, 3, 6);
+        if (_board[1]) checkTriplet(1, 4, 7);
+        if (_board[2]) checkTriplet(2, 5, 8);   
+        // check diagonals
+        if (_board[0]) checkTriplet(0, 4, 8);
+        if (_board[2]) checkTriplet(2, 4, 6);
+        // check draw
+        if (!_board.includes(null)) gameManager.draw();
+    }
+
+    const checkTriplet = (num1, num2, num3) => {
+        if (_board[num1] == _board[num2] && _board[num1] == _board[num3]) {
+            gameManager.declareWinner();
+        }
+    }
+
     const clearBoard = () => {
-        htmlBoxes.forEach(box => {
+        _htmlBoxes.forEach(box => {
             if (box.firstChild) box.removeChild(box.firstChild);
         })
     }
+    const resetMarkers = () => {
+        _board = [null, null, null,
+                    null, null, null,
+                    null, null, null];
+        renderBoard();
+    }
     const renderBoard = () => {
         clearBoard();
-        for (let i=0; i < _markers.length; i++) {
+        for (let i=0; i < _board.length; i++) {
             const marker = document.createElement('div');
             marker.classList.add('marker');
-            marker.textContent = _markers[i];
-            htmlBoxes[i].append(marker);
+            marker.textContent = _board[i];
+            _htmlBoxes[i].append(marker);
         }
     }
-    return {renderBoard};
+    return {clearBoard, renderBoard, resetMarkers};
 })();
 
 const Player = (name) => {
@@ -40,5 +70,16 @@ const Player = (name) => {
 
 const gameManager = (() => {
     const player = Player();
-    return {player}
+    const win = () => {
+        document.querySelector('.header').textContent = player.getMarker() + ' wins!';  
+    }
+    const draw = () => {
+        document.querySelector('.header').textContent = 'Draw!';
+    }
+    const resetGame = () => {
+        document.querySelector('.header').textContent = 'TIC-TAC-TOE';
+        gameBoard.resetMarkers();
+    }
+    document.querySelector('.reset-button').addEventListener("click", resetGame);
+    return {player, declareWinner: win, resetGame, draw}
 })();
